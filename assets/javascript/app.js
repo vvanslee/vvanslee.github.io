fd4a97246fe4586a3a60eb160d83194fe2a3d31e
vvanslee <vvanslee@gmail.com>
2017-12-12 22:17:03 -0600

    updated resume and added pdf

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,27 +1,26 @@
 $(document).ready(function() {
-    $('#mailbutton').click(function(event) {
-      window.location = "mailto:vvanslee@gmail.com";
-    });
+  $('#mailbutton').click(function(event) {
+    window.location = "mailto:vvanslee@gmail.com";
   });
+});
 
 $(document).ready(function(){
-    // Add smooth scrolling to all links
-    $("a").on('click', function(event) {
-  
-      // Make sure this.hash has a value before overriding default behavior
-      if (this.hash !== "") {
-        // Prevent default anchor click behavior
-        event.preventDefault();
-  
-        // Store hash
-        var hash = this.hash;
-  
-        // Using jQuery's animate() method to add smooth page scroll
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 1400, function(){
-          window.location.hash = hash;
-        });
-      } // End if
-    });
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1400, function(){
+        window.location.hash = hash;
+      });
+    } // End if
   });
+});
